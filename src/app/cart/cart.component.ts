2340ec8195bea889f57d56c91e1e1e00f2971ea6
ElodiePessy <elodie.pessy@gmail.com>
2020-01-21 11:23:51 +0100

    modif 4

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+items;
+ checkoutForm;
+
+  constructor(
+    private cartService: CartService,
+     private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+     this.items = this.cartService.getItems();
   }
 
 }
