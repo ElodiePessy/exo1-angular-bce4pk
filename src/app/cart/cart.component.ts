d1ecc7789840f0ba84fb8b0c29b676c64ef83f07
ElodiePessy <elodie.pessy@gmail.com>
2020-01-21 11:28:06 +0100

    modif5

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -16,8 +16,22 @@ items;
   constructor(
     private cartService: CartService,
      private formBuilder: FormBuilder,
-  ) { }
+  ) { 
+    this.items = this.cartService.getItems();
 
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+ onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
+ 
   ngOnInit() {
      this.items = this.cartService.getItems();
   }
